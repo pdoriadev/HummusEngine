dcb2e2bffb07369c942ce7f2b37a502759101a97
Pita Bread <pdoriadev@gmail.com>
2020-08-11 22:53:53 -0700

    - Removed Macro Definitions example I typed up for my C++ documentation. Works as before.

--- HummusEngine/Hummus/EntryPoint.h
+++ HummusEngine/Hummus/EntryPoint.h
@@ -6,19 +6,12 @@
 
 extern Hummus::Application* Hummus::CreateApplication();
 
-#define NUMBER_FIVE 5
-
 int main(int argc, char** argv)
 {
 
-	printf("NUMBER_FIVE = %d ", NUMBER_FIVE );
-
-
-
-
-	//Hummus::Log::Init();
-	//Hummus::Log::GetCoreLogger()->warn("Iniitialized Core Logger!"); 
-	//(*Hummus::Log::GetClientLogger()).info("Initialized Client Logger"); 
+	Hummus::Log::Init();
+	Hummus::Log::GetCoreLogger()->warn("Iniitialized Core Logger!"); 
+	(*Hummus::Log::GetClientLogger()).info("Initialized Client Logger"); 
 
 	auto app = Hummus::CreateApplication();
 	app->Run();
